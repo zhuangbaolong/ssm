43055fda6e60065f43350c11e883f4a98e974a88
zhuangbl-bitware <zhuangbaolong@bit-ware.cn>
2023-06-16 17:30:12 +0800

    update Controller

--- springmvc_01_quickstart/src/main/java/com/test/controller/UserController.java
+++ springmvc_01_quickstart/src/main/java/com/test/controller/UserController.java
@@ -3,72 +3,82 @@ package com.test.controller;
 import com.test.entity.User;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 //2.controller
-@Controller
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     @RequestMapping("/commonParam")
     //设置当前返回值类型
-    @ResponseBody
-    public String commonParam(String name){
-        System.out.println("user: "+name);
-        return "{'module':"+name +"'common param'}";
+    public String commonParam(String name) {
+        System.out.println("user: " + name);
+        return "{'module':" + name + "'common param'}";
     }
+
     //日期类型传参
     @RequestMapping("/dateParam")
-    @ResponseBody
-    public String dateParam(@DateTimeFormat(pattern = "yyyy-MM-dd") Date date){
-        System.out.println("date(yyyy-mm-dd): "+date);
-        return "{'module':"+date +"'date param'}";
+    public String dateParam(Date date,
+                            @DateTimeFormat(pattern = "yyyy-MM-dd") Date date1,
+                            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date date2) {
+        System.out.println("date(默认): " + date);
+        System.out.println("date(yyyy-mm-dd): " + date1);
+        System.out.println("date(yyyy-mm-dd HH:mm:ss): " + date2);
+        return "{'module':" + date + "'date param'}";
     }
 
     //pojo传参
     @RequestMapping("/pojoParam")
-    @ResponseBody
-    public String pojoParam(User user){
-        System.out.println("user: "+user);
-        return "{'module':"+user +"'date param'}";
+    public String pojoParam(User user) {
+        System.out.println("user: " + user);
+        return "{'module':" + user + "'date param'}";
     }
 
     //实体类嵌套实体类
     @RequestMapping("/pojoContainPojoParam")
-    @ResponseBody
-    public String pojoContainPojoParam(User user){
-        System.out.println("user: "+user);
-        return "{'module':"+user +"'date param'}";
+    public String pojoContainPojoParam(User user) {
+        System.out.println("user: " + user);
+        return "{'module':" + user + "'date param'}";
     }
 
     //数组传参
     @RequestMapping("/arrayParam")
-    @ResponseBody
-    public String arrayParam(String[] likes){
-        System.out.println("user: "+ Arrays.toString(likes));
-        System.out.println("user: "+ likes);
-        return "{'module':"+likes +"'date param'}";
+    public String arrayParam(String[] likes) {
+        System.out.println("user: " + Arrays.toString(likes));
+        System.out.println("user: " + likes);
+        return "{'module':" + likes + "'date param'}";
     }
+
     //集合参数
     @RequestMapping("/listParam")
-    @ResponseBody
-    public String listParam(@RequestParam List<String> likes){
-        System.out.println("user: "+ likes);
-        return "{'module':"+likes +"'date param'}";
+    public String listParam(@RequestParam List<String> likes) {
+        System.out.println("user: " + likes);
+        return "{'module':" + likes + "'date param'}";
     }
 
     //json
     @RequestMapping("/listParamForJson")
-    @ResponseBody
-    public String listParamForJson(@RequestBody List<String> likes){
-        System.out.println("user: "+ likes);
-        return "{'module':"+likes +"'date param'}";
+    public String listParamForJson(@RequestBody List<String> likes) {
+        System.out.println("user: " + likes);
+        return "{'module':" + likes + "'date param'}";
     }
 
+    //pojo参数：json
+    @RequestMapping("/pojoParamForJson")
+    public String pojoParamForJson(@RequestBody User user) {
+        System.out.println("user: " + user);
+        return "{'module':" + user + "'date param'}";
+    }
+
+    //多个实体类:json
+    @RequestMapping("/listPojoParamForJson")
+    public String listPojoParamForJson(@RequestBody List<User> userList) {
+        System.out.println("user: " + userList);
+        return "{'module':" + userList + "'date param'}";
+    }
 }
