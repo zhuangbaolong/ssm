97b77d36c78685d9e1fd647728afaf37205c166a
zhuangbl-bitware <zhuangbaolong@bit-ware.cn>
2023-08-08 17:28:54 +0800

    开启缓存功能

--- springboot_ehcache/src/main/java/com/example/springboot_ehcache/SpringbootEhcacheApplication.java
+++ springboot_ehcache/src/main/java/com/example/springboot_ehcache/SpringbootEhcacheApplication.java
@@ -1,9 +1,13 @@
 package com.example.springboot_ehcache;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+//开启缓存功能,不然@Cacheable用不了
+@EnableCaching
 public class SpringbootEhcacheApplication {
 
     public static void main(String[] args) {
--- springboot_ehcache/src/main/java/com/example/springboot_ehcache/domain/SMSCode.java
+++ springboot_ehcache/src/main/java/com/example/springboot_ehcache/domain/SMSCode.java
@@ -2,8 +2,10 @@ package com.example.springboot_ehcache.domain;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class SMSCode {
+public class SMSCode implements Serializable {
     private String tele;
     private String code;
 }
