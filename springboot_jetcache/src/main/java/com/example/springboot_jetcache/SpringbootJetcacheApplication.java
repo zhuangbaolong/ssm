a66916c08ae54ca09bf845741df923b680a3bd92
zhuangbaolong <3294402246@qq.com>
2023-08-17 23:20:05 +0800

    jetcache远程缓存

--- springboot_jetcache/src/main/java/com/example/springboot_jetcache/SpringbootJetcacheApplication.java
+++ springboot_jetcache/src/main/java/com/example/springboot_jetcache/SpringbootJetcacheApplication.java
@@ -1,9 +1,12 @@
 package com.example.springboot_jetcache;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//jetcache启用缓存
+@EnableCreateCacheAnnotation
 public class SpringbootJetcacheApplication {
 
     public static void main(String[] args) {
--- springboot_jetcache/src/main/java/com/example/springboot_jetcache/service/Impl/SMSCodeServiceImpl.java
+++ springboot_jetcache/src/main/java/com/example/springboot_jetcache/service/Impl/SMSCodeServiceImpl.java
@@ -1,33 +1,40 @@
 package com.example.springboot_jetcache.service.Impl;
 
 
+import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.anno.CreateCache;
 import com.example.springboot_jetcache.domain.SMSCode;
 import com.example.springboot_jetcache.service.SMSCodeService;
 import com.example.springboot_jetcache.utils.CodeUtils;
+import org.checkerframework.checker.mustcall.qual.CreatesMustCallFor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 //核心业务：验证码
 @Service
-public class SMSCodeServiceImpl  implements SMSCodeService {
+public class SMSCodeServiceImpl implements SMSCodeService {
 
     @Autowired
     private CodeUtils codeUtils;
-    
+
+    //缓存空间
+    @CreateCache(name = "jetcache", area = "sms", expire = 3600, timeUnit = TimeUnit.SECONDS)
+    private Cache<String, String> jetCache;
+
     @Override
     //放缓存
-    @CachePut(value = "smsCode",key = "#tele")
     public String sendCodeToSMS(String tele) {
         String generator = codeUtils.generator(tele);
+        jetCache.put("tele", generator);
         return generator;
     }
 
     @Override
     public boolean checkCode(SMSCode smsCode) {
-        String code = smsCode.getCode();
-        String cacheCode = codeUtils.get(smsCode.getTele());
-        System.out.println(cacheCode);
-        return code.equals(cacheCode);
+        String code = jetCache.get(smsCode.getTele());
+        return code.equals(smsCode.getCode());
     }
 }
