0d506c5956c4b38f3707b68457c05363f01e4a2e
zhuangbaolong <3294402246@qq.com>
2023-08-31 22:55:52 +0800

    amqp_rabbitmq_topic_1

---
+++ springboot_rabbitmq/src/main/java/com/example/springboot_rabbitmq/service/impl/rabbitmq/topic/config/RabbitTopicConfig.java
@@ -0,0 +1,38 @@
+package com.example.springboot_rabbitmq.service.impl.rabbitmq.topic.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitTopicConfig {
+    //队列
+    @Bean
+    public Queue topicQueue(){
+        /**
+         * durable:是否持久化，默认false
+         * exclusive:是否当前连接自用,默认false，连接关闭队列关闭
+         * autoDelete:是否自动删除，当生产者或消费者不再使用此队列，自动删除
+         */
+        return new Queue("topic_queue",true,false,false);
+    }
+
+    @Bean
+    public Queue topicQueue2(){
+        return new Queue("topic_queue2");
+    }
+    //直连交换机
+    @Bean
+    public TopicExchange topicExchange(){
+        return new TopicExchange("topicExchange");
+    }
+    //绑定
+    @Bean
+    public Binding bindingTopic(){
+        return BindingBuilder.bind(topicQueue()).to(topicExchange()).with("topic.order.id");
+    }
+    @Bean
+    public Binding bindingTopic2(){
+        return BindingBuilder.bind(topicQueue2()).to(topicExchange()).with("topic2");
+    }
+}
---
+++ springboot_rabbitmq/src/main/java/com/example/springboot_rabbitmq/service/impl/rabbitmq/topic/listener/MessageListener.java
@@ -0,0 +1,18 @@
+package com.example.springboot_rabbitmq.service.impl.rabbitmq.topic.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+    //监听器
+    @RabbitListener(queues = "topic_queue")
+    public void receive(String id){
+        System.out.println("处理：已完成短信业务（rabbitmq topic 1）.id"+id);
+    }
+    @RabbitListener(queues = "topic_queue2")
+    public void receive2(String id){
+        System.out.println("处理：已完成短信业务（rabbitmq topic 2）.id"+id);
+    }
+}
