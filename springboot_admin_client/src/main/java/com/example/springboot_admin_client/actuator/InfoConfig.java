883feef881928581f845861123c3156dca6b836c
zhuangbaolong <3294402246@qq.com>
2023-09-03 22:21:31 +0800

    info端点

---
+++ springboot_admin_client/src/main/java/com/example/springboot_admin_client/actuator/InfoConfig.java
@@ -0,0 +1,20 @@
+package com.example.springboot_admin_client.actuator;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class InfoConfig implements InfoContributor {
+    @Override
+    public void contribute(Info.Builder builder) {
+        //动态数据处理
+        builder.withDetail("runTime",System.currentTimeMillis());
+        Map infoMap = new HashMap();
+        infoMap.put("buildTime","2023-9-3");
+        builder.withDetails(infoMap);
+    }
+}
